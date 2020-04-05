f9c28afe2e03e4fc5f2b1dfe04fb43d2afab11d1
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-05 18:01:34 -0400

    Clean Up/Refactor -Remove console logs -Change lifycycle function and removed space

--- src/allData.js
+++ src/allData.js
@@ -4,37 +4,29 @@ class AllData extends Component {
   state = {
     theData: 0, 
     isLoaded: false,
-    thestate: "Choose a state."
+    thestate: ""
   };
 
   async componentDidMount() {
-    try {
       const response = await fetch(`https://corona.lmao.ninja/states`);
       const data = await response.json();
+      const statesArray1 = [];
       this.setState({
         theData: data,
-        isLoaded: true,
-        thestate: ""
-        
+        isLoaded: true
       });
-    } catch (error) {
-      console.error("Error", error);
-      return error;
     }
-  }
 
   handleClick = async (event) => {
     event.preventDefault();
-    console.log("Updated State: ", this.state.thestate)
     const appendState = this.state.thestate;
     return appendState;
   };
 
-
   render() {
     const {isLoaded} = this.state;
     if(isLoaded === false){
-      return <div></div>;
+      return <></>;
     }else{
     const theElement = this.state.thestate;
     const arrayOfStates = [];
@@ -43,28 +35,25 @@ class AllData extends Component {
       if (element.state === theElement){
         arrayOfStates.push(element.state)
         arrayOfCases.push(element.cases)
-        console.log("The States Array: ",arrayOfStates)
-        console.log("The Cases Array: ", arrayOfCases)
       }
     }
       ); 
-    return (
-      <div>
-        <h2>Select Your State</h2>
-        <p>{this.state.thestate}</p>
-        <form onChange={(e) => this.setState({ thestate: e.target.value })}>
-          <select>
-            {this.state.theData.map(element => 
-            <option key={element.state} thestate={element.state}>{element.state}</option>
-            )}
-          </select>
-          <input type="submit" onClick={this.handleClick} />
-          {console.log("lower log US State: ",this.state.theData[0])}
-        </form>
-      </div>
-    );
+      return (
+        <div>
+          <h2>Select Your State</h2>
+          <p>{this.state.thestate}</p>
+          <form onChange={(e) => this.setState({ thestate: e.target.value })}>
+            <select>
+              {this.state.theData.map(element => 
+              <option key={element.state} value={element.state}>{element.state}</option>
+              )}
+            </select>
+            <input type="submit" onClick={this.handleClick} />
+          </form>
+        </div>
+      );
+    }
   }
-}
 }
 
 export default AllData;
