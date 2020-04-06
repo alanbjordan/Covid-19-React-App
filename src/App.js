eaa16f107b4d269b10ea09864d79633fc629f63c
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-05 21:42:05 -0400

    Added new routes Added nav menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import StateData from './components/StateData';
 import AllStates from './components/AllStates';
+import HomePage from './components/HomePage';
+import AllCountries from './components/AllCountries';
 import './App.css';
 import {
-  Route,
+  Route, Link,
   BrowserRouter as Router
 } from 'react-router-dom';
 
@@ -12,12 +14,18 @@ const App = () => {
     // const dataUp = "covid19"
     return (
       <div>
-        <h1>US Covid19 Current Data</h1>
+          <nav>
+            <a href="/">Home</a> <br></br>
+            <a href="/states">Data By U.S. State</a><br></br>
+            <a href="/country">Data By Country</a>              
+          </nav>
+        <h1>COVID-19 Data</h1>
         <Router>
-          <Route exact path='/' component={AllStates} />
+          <Route exact path='/'component={HomePage}/>
+          <Route exact path='/country' component={AllCountries}/>
+          <Route  exact path='/states'  component={AllStates} />
           <Route exact path='/states/:state?' component={StateData} />
         </Router>
-
       </div>
     );
   }
