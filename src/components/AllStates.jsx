025edef92445a41447fd06d3a58ca7cabd951bc1
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-14 23:50:19 -0400

    Add table

--- src/components/AllStates.jsx
+++ src/components/AllStates.jsx
@@ -29,19 +29,43 @@ class AllStates extends Component {
       };
 
     render() {
-        if (this.state.data != 0) {
+        if (this.state.data.length > 0) {
         const statesData = this.state.data;
-
+        console.log(statesData);
             return (
-                <div>
-                    {statesData.map(element => (
-                        <div key={element.state}>
-                            {element.state} <br></br>
-                            <Link to={`/states/${element.state}`}>Get Stats</Link>
-                        </div>
-                    ))}
-                </div>
-            )
+                <div className='allStatesWrapper'>
+                    <h1>United States of America COVID-19 Data</h1>                    
+                    <div className='allStates'>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>State</th>
+                                    <th>Total Cases</th>
+                                    <th>Today's Cases</th>
+                                    <th>Total Deaths</th>
+                                    <th>Today's Deaths</th>
+                                    <th>Active Cases</th>
+                                    <th>Total Test</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {statesData.map(element => (
+                                    <tr key={element.state}>
+                                        <td>{element.state}  <Link to={`/states/${element.state}`}> more info</Link></td>
+                                        <td>{element.cases}</td>
+                                        <td>{element.todayCases}</td>
+                                        <td>{element.deaths}</td>
+                                        <td>{element.todayDeaths}</td>
+                                        <td>{element.active}</td>
+                                        <td>{element.tests}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                </div> 
+            </div>
+        )
         } else {
             return <div>Loading Data....</div>
         }        
