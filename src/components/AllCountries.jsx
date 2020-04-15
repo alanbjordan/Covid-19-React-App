bd098d50c95c7290d17c39d7012b0b6c91b8ed45
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-14 23:50:08 -0400

    Add Table Import Link

--- src/components/AllCountries.jsx
+++ src/components/AllCountries.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import {
-//     Link,
-//   } from 'react-router-dom';
+import {
+    Link,
+  } from 'react-router-dom';
 
 class AllCountries extends Component {
     constructor() {
@@ -29,7 +29,47 @@ class AllCountries extends Component {
       };
 
     render() {
-        console.log("the country data => ", this.state.data);
+        const country = this.state.data;
+        if (country.length > 0) {
+          console.log(country);
+          return (
+            <div className='allCountriesWrapper'>
+              <h1>Global COVID-19 Data</h1> 
+              <div className='allStates'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>Total Cases</th>
+                            <th>Today's Cases</th>
+                            <th>Total Deaths</th>
+                            <th>Today's Deaths</th>
+                            <th>Recoveries</th>
+                            <th>Active Cases</th>
+                            <th>Critical Cases</th>
+                            <th>Total Tests</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {country.map(element => (
+                            <tr key={element.country}>
+                                <td>{element.country}  <Link to={`/country/${element.country}`}> more info</Link></td>
+                                <td>{element.cases}</td>
+                                <td>{element.todayCases}</td>
+                                <td>{element.deaths}</td>
+                                <td>{element.todayDeaths}</td>
+                                <td>{element.recovered}</td>
+                                <td>{element.active}</td>
+                                <td>{element.critical}</td>
+                                <td>{element.tests}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+              </div> 
+            </div>
+          )
+        }
         return <div>Loading Data....</div>
     }
 }
